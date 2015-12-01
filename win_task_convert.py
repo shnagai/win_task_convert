d0e52042bec149cf93b81dee7acd13aa14800072
shoichiro nagai <nagais@eac.ne.jp>
2015-12-01 19:23:51 +0900

    [add]Additional regular expression matching

--- win_task_convert.py
+++ win_task_convert.py
@@ -6,16 +6,13 @@
 from __future__ import print_function
 
 import os.path
-import subprocess
 import glob
-import string
+import re
 
 src_path = '/Users/nagais/tmp/xml_lists_b1/work'
 dst_path = '/Users/nagais/tmp/xml_lists_b1/out'
-
 file_list = glob.glob(os.path.join(src_path, '*'))
 file_name = [os.path.relpath(x, src_path) for x in file_list]
-
 match1 = 'version="1.1"'
 rep1 = 'version="1.2"'
 match2 = '</Principal>'
@@ -30,9 +27,12 @@ def main():
         f_sedfile = open(sedfile, "r")
         f_addfile = open(addfile, "w")
         for line in f_sedfile:
-            line = line.replace(match1, rep1)
-            line = line.replace(match2, rep2)
-            line = line.replace(match3, rep3)
+            # string replace
+            # line = line.replace(match1, rep1)
+            # line = line.replace(match2, rep2)
+            # line = line.replace(match3, rep3)
+            # regexp ver
+            line = re.sub(r'^\s\s\s\s<Enabled>true</Enabled>', r'    <Enabled>false</Enabled>', line)
             f_addfile.write(line)
         f_sedfile.close()
         f_addfile.close()
